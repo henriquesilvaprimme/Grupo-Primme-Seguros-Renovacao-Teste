46e6a1167d25b871ab97cee9ce04aa422864daed
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-10-03 08:36:16 -0300

    Update Usuarios.jsx

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -16,7 +16,7 @@ const GerenciarUsuarios = () => {
   const fetchUsuariosFromSheet = async () => {
     setError(null);
     try {
-      const response = await fetch(`${GOOGLE_SHEETS_BASE_URL}?v=pegar_usuario_renovacao`);
+      const response = await fetch(`${GOOGLE_SHEETS_BASE_URL}?v=pegar_usuario`);
       
       if (!response.ok) {
         throw new Error(`Erro HTTP: ${response.status} - ${response.statusText}`);
