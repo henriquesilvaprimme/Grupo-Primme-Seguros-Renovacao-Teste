c2338199e73fd384f9fd23b51b829c0b897f3625
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-01 17:21:41 -0300

    Update Lead.jsx

--- src/components/Lead.jsx
+++ src/components/Lead.jsx
@@ -188,7 +188,7 @@ const Lead = ({ lead, onUpdateStatus, disabledConfirm, isAdmin }) => {
           <option value="Fechado">Fechado</option>
           <option value="Perdido">Perdido</option>
           <option value="Sem Contato">Sem Contato</option>
-          <option value="Apolice Cancelada">Apolice Cancelada</option>
+          <option value="Cancelado">Apolice Cancelada</option>
         </select>
 
         {/* Lógica condicional para exibir Confirmar ou Alterar */}
