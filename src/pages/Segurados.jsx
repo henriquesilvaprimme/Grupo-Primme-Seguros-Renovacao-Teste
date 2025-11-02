126148d18253115ec1c8a9f7a9f515c4b49a9170
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-02 20:16:35 -0300

    Update Segurados.jsx

--- src/pages/Segurados.jsx
+++ src/pages/Segurados.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Search, Phone, Calendar, Shield, User, AlertCircle, Car, Edit, X, CheckCircle , X } from 'lucide-react';
+import { Search, Phone, Calendar, Shield, User, AlertCircle, Car, Edit, X, CheckCircle } from 'lucide-react';
 
 const GOOGLE_APPS_SCRIPT_BASE_URL = 'https://script.google.com/macros/s/AKfycbyGelso1gXJEKWBCDScAyVBGPp9ncWsuUjN8XS-Cd7R8xIH7p6PWEZo2eH-WZcs99yNaA/exec';
 
