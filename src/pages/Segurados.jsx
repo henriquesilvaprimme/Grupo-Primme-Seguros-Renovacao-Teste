fff64df6501a1adfd52bce37cf98432daad7935a
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-03 21:23:47 -0300

    Update Segurados.jsx

--- src/pages/Segurados.jsx
+++ src/pages/Segurados.jsx
@@ -491,7 +491,7 @@ const Segurados = () => {
                   <input
                     type="text"
                     value={endossoData.vehicleYearModel}
-                    onChange={(e) => setEndossoData({ ...endossoData, vehicleYearModel: e.target.value })}\
+                    onChange={(e) => setEndossoData({ ...endossoData, vehicleYearModel: e.target.value })}
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   />
                 </div>
